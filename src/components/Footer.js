6fc05b5962c70b0c4628e079cbe8561fbe8180d2
rakesh-u09 <rakesh9901182@gmail.com>
2022-07-08 00:16:34 +0530

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div style={{"height":"25%"}}>
+        
+        <footer className="text-center text-lg-start" style={{"background-color": "blue"}}>
+   <p style={{"textAlign":"center","color":"white","padding":"1%"}}>Design and Developed by Rakesh U and Naresh Shet</p>
+   <p style={{"textAlign":"center"}}></p>
+  </footer>
+    </div>
+  )
+}
